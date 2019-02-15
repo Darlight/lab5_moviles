bcc145256c4d878a11b2effbaf234c029e5c1697
Darlight <andres.perdomo09@gmail.com>
2019-02-15 10:37:55 -0600

    Mitad del trabajo

---
+++ app/src/main/java/com/example/marioandres/playermusic/MusicService.java
@@ -0,0 +1,60 @@
+package com.example.marioandres.playermusic;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import java.util.ArrayList;
+import android.content.ContentUris;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.PowerManager;
+import android.util.Log;
+
+public class MusicService extends Service implements
+        MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener,
+        MediaPlayer.OnCompletionListener {
+    //media player
+    private MediaPlayer player;
+    //song list
+    private ArrayList<Song> songs;
+    //current position
+    private int songPosn;
+    public void onCreate(){
+        //create the service
+        super.onCreate();
+        //initialize position
+        songPosn=0;
+        //create player
+        player = new MediaPlayer();
+        initMusicPlayer();  
+    }
+    public void initMusicPlayer(){
+        //set player properties
+        player.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        return false;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+
+    }
+}
